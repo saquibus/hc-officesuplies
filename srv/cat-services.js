6721dee623e0dcd5061bd8dd506834dcd915a4ac
Saul Antonio Quintero Busigo <squinterobusigo@deloitte.es>
2023-09-27 10:50:14 +0000

    Localitation commit

--- srv/cat-services.js
+++ srv/cat-services.js
@@ -13,4 +13,20 @@ module.exports = cds.service.impl(function () {
             this.emit('prod_Change', header)
         })
     })
+    this.on('get_supplier_info', async () => {
+        try {
+            const db = await cds.connect.to('db')
+            const dbClass = require("sap-hdbtext-promisfield")
+            let dbConn = new dbClass(await dbClass.createConnection(db.options.credentials))
+            const hdbext = require("@sap/hdbext")
+            const sp = await dbConn.loadProcedurePromisifield(hdbext,null, 'getsupplier_info')
+            const output = await dbConn.callProcedurePromisifield(sp,[])
+            console.log(output.results)
+            return output.results
+
+        } catch (error) {
+            console.error(error)
+            return
+        }
+    })
 })
